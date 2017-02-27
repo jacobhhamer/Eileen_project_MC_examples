699a78ef3ddb6a51468b2adeabd95d9132f31b52
ECGonzales <gonza338@gmail.com>
2017-02-27 12:41:36 -0500

    Updated to allow bettor not to go into negatives. Added docstrings for each function

--- MCdice.py
+++ MCdice.py
@@ -5,64 +5,66 @@ import matplotlib.pyplot as plt
 import time
 import smallcodes as sc
 
-#Globally define some variables
+# Globally define some variables to be used with multiple bettor strategies
 samplesize = 100
 startingFunds = 10000
 wagerSize = 100
-wagerCount = 10000
+wagerCount = 1000
 
-'''
-Function to represent rolling the dice.
-'''
+
 def rollDice():
+    """
+    Function to represent rolling the dice. House wins if roll 100 or 1-50. You win if roll between 51 and 99.
+    """
+
     roll = random.randint(1, 100)
 
     if roll == 100:
-        #print(roll, 'roll was 100, you lose. What are the odds?! Play again!')
         return False
     elif roll <= 50:
-        #print(roll, 'roll was 1-50, you lose.')
         return False
     elif 100 > roll > 50:
-        #print(roll, 'roll was 51-99, you win! *flashing lights! play more*')
         return True
 
 
-'''
-Making a more complicated bettor (Martingale Strategy)
-'''
+def dbl_bettor(funds, inital_wager, wager_count, color):
+    """
+    Making a more complicated bettor (Martingale Strategy). Double your wager when you lose and go back to original
+    wager when you win again.
+    """
 
-def dbl_bettor(funds,inital_wager,wager_count):
-    #global broke_count
+    # global broke_count
     value = funds
     wager = inital_wager
     wX = []
     vY = []
     currentWager = 1
-
-    # We will be betting on the previous outcome therefore, #
-    previousWager = 'win'
+    previousWager = 'win'  # We are betting on the previous outcome. Assume start with a win.
     previousWagerAmount = inital_wager
 
     while currentWager <= wager_count:
         if previousWager == 'win':
-            if rollDice():  #rollDice returned True
+            if rollDice():  # rollDice returned True
                 value += wager
                 wX.append(currentWager)
                 vY.append(value)
-            else:   #rollDice returned False
+            else:  # rollDice returned False
                 value -= wager
                 previousWager = 'loss'
                 previousWagerAmount = wager
                 wX.append(currentWager)
                 vY.append(value)
-                if value < 0:
-                    #broke_count += 1
+                if value <= 0:
+                    # broke_count += 1
                     break
         elif previousWager == 'loss':
-            #print('We lost last one, so we will be smart and dbl!')
+            # print('We lost last one, so we will be smart and dbl!')
             if rollDice():
                 wager = previousWagerAmount * 2
+
+                if (value - wager) < 0:
+                    wager = value
+
                 value += wager
                 wager = inital_wager
                 previousWager = 'win'
@@ -70,22 +72,24 @@ def dbl_bettor(funds,inital_wager,wager_count):
                 vY.append(value)
             else:
                 wager = previousWagerAmount * 2
-                #print('We lost', wager)
+                # print('We lost', wager)
+                if (value - wager) < 0:  # Making it so we bet all that was left. Can't go negative.
+                    wager = value
                 value -= wager
                 previousWager = 'loss'
                 previousWagerAmount = wager
                 wX.append(currentWager)
                 vY.append(value)
-                if value < 0:
-                    #print('We broke. Went broke after',currentWager,'bets')
-                    #broke_count += 1
+                if value <= 0:
+                    # print('We broke. Went broke after',currentWager,'bets')
+                    # broke_count += 1
                     break
-
 
         currentWager += 1
 
-    #print(value)
-    plt.plot(wX,vY, 'c') #plot this bettor in cyan
+    # print(value)
+    plt.plot(wX, vY, color)
+
 
 '''
 Adding some more statistics
@@ -110,34 +114,36 @@ sc.stop() #stops code from here on out. Not interactive like IDL.
 #time.sleep(555) #pauses code for set amount of seconds.
 '''
 
-'''
-Simple Bettor, betting the same amount each time. The while loop rolls the dice wager_count times and adds or subtracts
-from the bettor's funds.
-'''
-def simple_bettor(funds, initial_wager, wager_count,color):
-    #global broke_count
+
+def simple_bettor(funds, initial_wager, wager_count, color):
+    """
+    Simple Bettor, betting the same amount each time. The while loop rolls the dice wager_count times and adds or subtracts
+    from the bettor's funds.
+    """
+
+    # global broke_count
     value = funds
     wager = initial_wager
+    wX = []  # wager X
+    vY = []  # value y
+    currentWager = 1  # wager count starting at one
 
-    wX = [] # wager X
-    vY = [] # value y
-    currentWager = 1  #wager count starting at one
-
-    while currentWager <= wager_count: #Run while current wager is less than wager_count(here it is 100)=>run only 100 times
+    while currentWager <= wager_count:  # Run while currentWager is less than wagerCount (defined with global variables)
         if rollDice():
-            value += wager  #If you win, add to funds
+            value += wager  # If you win, add to funds
             wX.append(currentWager)
             vY.append(value)
-            #print(value)
+            # print(value)
         else:
-            value -= wager  #lose subtract bet from funds
+            value -= wager  # lose subtract bet from funds
             wX.append(currentWager)
             vY.append(value)
-            #print(value)
+            # print(value)
 
-            if value < 0:
-                currentWager += 10000000000000
-                #broke_count += 1
+            if value <= 0:
+                #currentWager += 10000000000000
+                break
+                # broke_count += 1
 
         currentWager += 1
 
@@ -163,16 +169,17 @@ plt.title('MC Dice: 100 trials of length 1000')
 plt.show()
 '''
 
+
 '''Now run with both bettors to compare which is better.'''
 x = 0
 
 while x < samplesize:
-    simple_bettor(startingFunds, wagerSize, wagerCount, 'k')
-    simple_bettor(startingFunds,wagerSize*2, wagerCount, 'c')
-    #dbl_bettor(startingFunds, wagerSize,wagerCount)
+    simple_bettor(startingFunds, wagerSize, wagerCount, 'k')  # K is black for matplotlib
+    #simple_bettor(startingFunds, wagerSize * 2, wagerCount, 'g')
+    dbl_bettor(startingFunds, wagerSize,wagerCount, 'c')
     x += 1
 
-plt.axhline(0, color= 'r')
+plt.axhline(0, color='r')
 plt.ylabel('Account value')
 plt.xlabel('Wager Count')
 plt.show()
